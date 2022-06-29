8d3cc0bbef260f474366b4402ef1fa7304d98868
Franciel Abreu <francielabreu23@gmail.com>
2022-06-28 21:40:46 -0300

    criação da rota que usa a api via cep

--- routes/web.php
+++ routes/web.php
@@ -2,10 +2,14 @@
 
 use App\Http\Controllers\Controller;
 use App\Http\Controllers\UserController;
+use App\Http\Controllers\ViaCepController;
 use Illuminate\Support\Facades\Route;
 
 
-Route::get('/users', [UserController::class, 'index'])->name('users.index');
+//Route::get('/users', [UserController::class, 'index'])->name('users.index');
  
+Route::get('/viacep', [ViaCepController::class, 'index'])->name('viacep.index');
+Route::post('/viacep', [ViaCepController::class, 'index'])->name('viacep.index.post');
+Route::get('/viacep/{cep}', [ViaCepController::class, 'show'])->name('viacep.show');
 
 
