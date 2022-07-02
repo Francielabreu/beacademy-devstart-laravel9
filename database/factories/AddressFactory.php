c69ca5c3e04295c8b3dddeb9e3f38649fad04bc3
Franciel Abreu <francielabreu23@gmail.com>
2022-07-01 23:53:33 -0300

    implementação do factory e do seeders para o laravel criar dados que vamos utilizar como exemplo

---
+++ database/factories/AddressFactory.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class AddressFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+
+        //cria elementos no banco para usarmos
+        return [
+            'address'=> $this->faker->address(),
+            'postal_code'=> $this->faker->postcode(),
+            'city'=> $this->faker->city(),
+            'country'=> $this->faker->country(),
+
+        ];
+    }
+}
