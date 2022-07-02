66e8289b1c24dbc50afe3f3f1b9d9ba08b7d442c
Franciel Abreu <francielabreu23@gmail.com>
2022-07-01 22:19:56 -0300

    criação dos metodos que valida o cep inserido pelo usuario e passa os parametros necessarios para mostrar na view

--- app/Http/Controllers/ViaCepController.php
+++ app/Http/Controllers/ViaCepController.php
@@ -3,18 +3,28 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Http;
 
 class ViaCepController extends Controller
 {
     public function index(Request $request)
     {
-        
-       
-        if ($request->cep) {
+       //recebe o paramero passado pelo usuario, para passar como parametro no metodo SHOW
+    
+      if ($request->cep)
             return redirect('/viacep/'.$request->cep);
 
-            return view('viacep.index');
-            dd('dd');
-        }
+        return view('viacep.index');
+            
+      
     }
+
+    public function show($cep)
+    {
+        
+        $response = Http::get('https://viacep.com.br/ws/'.$cep.'/json/')->json();
+        dd($response);
+
+    }
+
 }
