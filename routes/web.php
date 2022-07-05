5fcb7d6fa3297a9976f2f7fed730f411685ce969
Franciel Abreu <francielabreu23@gmail.com>
2022-07-05 09:18:47 -0300

    implementação das rotas de create

--- routes/web.php
+++ routes/web.php
@@ -5,9 +5,12 @@ use App\Http\Controllers\UserController;
 use App\Http\Controllers\ViaCepController;
 use Illuminate\Support\Facades\Route;
 
-
+Route::get('/users/create', [UserController::class, 'create'])->name('users.create');
+Route::post('/users',[UserController::class, 'store'])->name('users.store');
 Route::get('/users', [UserController::class, 'index'])->name('users.index');
 Route::get('/users/{id}', [UserController::class, 'show'])->name('users.show');
+
+
  
 Route::get('/viacep', [ViaCepController::class, 'index'])->name('viacep.index');
 Route::post('/viacep', [ViaCepController::class, 'index'])->name('viacep.index.post');
