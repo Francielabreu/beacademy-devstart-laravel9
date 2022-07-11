d5d787733ce4bb890de4a8c59702e4daed9f22e2
Franciel Abreu <francielabreu23@gmail.com>
2022-07-10 22:44:42 -0300

    implementação do campo de upload de imagem na view create e edit

--- resources/views/users/create.blade.php
+++ resources/views/users/create.blade.php
@@ -1,12 +1,9 @@
 @extends('template.users')
 @section('title', 'Cadastrar Usuario')
-
-
 @section('content')
     <h1>Cadastrar novo Usuario</h1>
 
     @if ($errors->any())
-
         <div class="alert alert-danger" role="alert">
             @foreach ($errors->all() as $error)
                 {{ $error }}<br>
@@ -15,30 +12,27 @@
 
     @endif
 
-    <form action="{{ route('users.store') }}" method="POST">
+    <form action="{{ route('users.store') }}" method="POST" enctype="multipart/form-data">
         @csrf
 
-
-
         <div class="mb-3">
+
+            <div class="mb-3">
+                <label for="image" class="form-label">Image</label>
+                <input type="file" id="image" name="image" class="form-control">
+            </div>
             <label for="name" class="form-label">Nome</label>
             <input type="text" id="name" name="name" class="form-control" placeholder="Informe seu nome">
         </div>
         <div class="mb-3">
             <label for="email" class="form-label">Email</label>
             <input type="email" id="email" name="email" class="form-control" placeholder="Informe seu email">
-
         </div>
         <div class="mb-3">
             <label for="senha" class="form-label">Senha</label>
             <input type="password" id="password" name="password" class="form-control" placeholder="Informe sua senha">
-
         </div>
 
         <button type="submit" class="btn btn-primary">Cadastrar</button>
-
     </form>
-
-
-
 @endsection
--- resources/views/users/edit.blade.php
+++ resources/views/users/edit.blade.php
@@ -1,37 +1,37 @@
 @extends('template.users')
 @section('title', 'Alterar Usuario')
 
-
 @section('content')
     <h1>Alterar Usuario {{ $user->name }}</h1>
     @if ($errors->any())
 
-    <div class="alert alert-danger" role="alert">
-        @foreach ($errors->all() as $error)
-            {{ $error }}<br>
-        @endforeach
-    </div>
+        <div class="alert alert-danger" role="alert">
+            @foreach ($errors->all() as $error)
+                {{ $error }}<br>
+            @endforeach
+        </div>
 
-@endif
+    @endif
 
     <form action="{{ route('users.update', $user->id) }}" method="POST">
         @method('PUT')
         @csrf
-        
-        
-            
-            <div class="mb-3">
-                <label for="name" class="form-label">Nome</label>
-                <input type="text" id="name" name="name" class="form-control" placeholder="Informe seu nome"  value="{{ $user->name }}">
-            </div>
-            <div class="mb-3">
-                <label for="email" class="form-label">Email</label>
-                <input type="email" id="email" name="email" class="form-control" placeholder="Informe seu email" value="{{ $user->email }}">
 
-            </div>
-               
-            <button type="submit" class="btn btn-primary">Atualizar</button>
-       
+        <div class="mb-3">
+            <label for="image" class="form-label">Image</label>
+            <input type="file" id="image" name="image" class="form-control">
+        </div>
+        <div class="mb-3">
+            <label for="name" class="form-label">Nome</label>
+            <input type="text" id="name" name="name" class="form-control" placeholder="Informe seu nome"
+                value="{{ $user->name }}">
+        </div>
+        <div class="mb-3">
+            <label for="email" class="form-label">Email</label>
+            <input type="email" id="email" name="email" class="form-control" placeholder="Informe seu email"
+                value="{{ $user->email }}">
+        </div>
+        <button type="submit" class="btn btn-primary">Atualizar</button>
     </form>
 
 
