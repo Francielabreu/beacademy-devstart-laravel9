85a908bbf452ef5cbb94ad6eaff81c3df69bf40a
Franciel Abreu <francielabreu23@gmail.com>
2022-07-11 22:37:52 -0300

    melhoria no template da aplicação

--- resources/views/template/users.blade.php
+++ resources/views/template/users.blade.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -7,11 +8,27 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet">
     <title>@yield('title')</title>
 </head>
-<body>
-    <div class="container w-50 mb-3">
-        @yield('content')
 
+<body>
+    <div class="container w-50 p-3">
         
+        <nav class="navbar navbar-expand-lg navbar navbar-dark bg-dark">
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav mr-auto">
+                    <li class="nav-item active">
+                        <a class="nav-link text-white" href="/users">Usuarios</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link text-white" href="/posts">Posts</a>
+                    </li>
+                </ul>
+            </div>
+        </nav>
+        @yield('content')
+    </div>
     
+
+
 </body>
+
 </html>
