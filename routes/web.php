44f8605b6ff91f436239d3d8cfecc2cc05d47555
Franciel Abreu <francielabreu23@gmail.com>
2022-07-11 22:34:40 -0300

    implementação da rota da chamada dos post

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,12 @@
 <?php
 
 use App\Http\Controllers\Controller;
+use App\Http\Controllers\PostController;
 use App\Http\Controllers\UserController;
 use App\Http\Controllers\ViaCepController;
 use Illuminate\Support\Facades\Route;
+
+Route::get('/posts', [PostController::class, 'index'])->name('posts.index');
 
 Route::get('/users/create', [UserController::class, 'create'])->name('users.create');
 Route::post('/users',[UserController::class, 'store'])->name('users.store');
@@ -12,6 +15,8 @@ Route::get('/users/{id}', [UserController::class, 'show'])->name('users.show');
 Route::get('/users/{id}/edit', [UserController::class, 'edit'])->name('users.edit');
 Route::put('/users/{id}', [UserController::class, 'update'])->name('users.update');
 Route::delete('/users/{id}', [UserController::class, 'destroy'])->name('users.destroy');
+
+
 
 
 
