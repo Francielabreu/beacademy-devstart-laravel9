16535764eae0b591fada21f35285347aba908137
Franciel Abreu <francielabreu23@gmail.com>
2022-07-11 22:38:05 -0300

    listagem das postagem

---
+++ resources/views/posts/index.blade.php
@@ -0,0 +1,37 @@
+@extends('template.users')
+@section('title', 'Listagem de Postagem')
+
+
+@section('content')
+
+    <h1>Listagem de Postagem</h1>
+    
+    <table class="table table-light table-striped table-hover">
+        <thead>
+            <tr>
+                <th scope="col">#ID</th>
+                <th scope="col">TITULO</th>
+                <th scope="col">POSTAGEM</th>
+                <th scope="col">DATA CADASTRO</th>
+                
+            </tr>
+        </thead>
+        <tbody>
+            @foreach ($posts as $post)
+                <tr>
+                    <th scope="row">{{ $post->id }}</th>
+                    <td>{{ $post->title }}</td>
+                    <td>{{ $post->post }}</td>
+                    <td>{{ date('d/m/Y - H:i', strtotime($post->created_at)) }}</td>
+                </tr>
+            @endforeach
+
+
+        </tbody>
+    </table>
+<div class="justify-content-center pagination color-black">
+   
+</div>
+    
+
+@endsection
